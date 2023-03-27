fdb9369deeb875d334500545ef6a80d28d3e6afb
Olakunle Paul <olawaleolalekan33@gmail.com>
2023-03-27 23:47:12 +0100

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
- * sum_them_all - sum all args
- * @n: integers to sum
- * Return: sum
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum;
-	va_list argSum;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-	va_start(argSum, n);
-	sum = 0;
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(argSum, int);
-	va_end(argSum);
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
+
 /**
- * print_numbers - prints numbers
- * @separator: seperator char
- * @n: const var
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list printArgs;
+	va_list nums;
+	unsigned int index;
 
-	va_start(printArgs, n);
-	for (i = 0; i < n; i++)
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
 	{
-		if (i > 0 && separator != NULL)
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		printf("%d", va_arg(printArgs, int));
 	}
-	va_end(printArgs);
+
 	printf("\n");
+
+	va_end(nums);
 }
